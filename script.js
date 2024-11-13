b86206fd01220bcbcb675d1e727d333e919efe47
Nomiusmani <usmanionline78@gmail.com>
2024-11-13 17:06:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var _a;
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var education = document.getElementById("education").value;
+    var experience = document.getElementById("experience").value;
+    var skills = document.getElementById("skills").value;
+    // Generate the resume content dynamically
+    var resumeOutput = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone No:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    ");
+    var resumeOutputElement = document.getElementById('resumeOutput');
+    if (resumeOutputElement) {
+        resumeOutputElement.innerHTML = resumeOutput;
+    }
+    else {
+        console.error("The resume display element is missing");
+    }
+});
---
+++ script.ts
@@ -0,0 +1,39 @@
+document.getElementById('resumeForm')?.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+    const name = (document.getElementById("name") as HTMLInputElement).value;
+    const email = (document.getElementById("email") as HTMLInputElement).value;
+    const phone = (document.getElementById("phone") as HTMLInputElement).value;
+    const education = (document.getElementById("education") as HTMLTextAreaElement).value;
+    const experience = (document.getElementById("experience") as HTMLTextAreaElement).value;
+    const skills = (document.getElementById("skills") as HTMLTextAreaElement).value;
+
+
+    // Generate the resume content dynamically
+    const resumeOutput = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Phone No:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+    `;
+
+    const resumeOutputElement = document.getElementById('resumeOutput');
+
+    if(resumeOutputElement){
+        resumeOutputElement.innerHTML=resumeOutput;
+    }else{
+        console.error("The resume display element is missing");
+
+    }
+})
